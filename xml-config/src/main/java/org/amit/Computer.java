218b33d44769887f646f3c6f83c6ffd1ce40a37d
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-18 00:12:13 +0530

    Create proper xml and inject dependencies accordingly and make proper method calls

--- xml-config/src/main/java/org/amit/Computer.java
+++ xml-config/src/main/java/org/amit/Computer.java
@@ -4,11 +4,33 @@ import org.amit.processor.Processor;
 import org.amit.ram.Ram;
 import org.amit.storage.Storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
     Ram rm;
     Storage str;
     Processor psr;
     String brand="";
+    List<String> ports=new ArrayList<>();
+
+
+    public List<String> getPorts() {
+        return ports;
+    }
+
+    public void setPorts(List<String> ports) {
+        this.ports = ports;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     Computer(Ram rm)
     {
         this.rm=rm;
@@ -27,12 +49,19 @@ public class Computer {
     public Processor getPsr() {
         return psr;
     }
+    public void init() {
+        System.out.println("Computer is assembled and ready!");
+    }
 
+    public void shutdown() {
+        System.out.println("Computer is shutting down...");
+    }
     public void config()
     {
         rm.getramtype();
         str.getstoragetype();
         psr.getprocessortype();
-        System.out.println("Computer class invoked");
+        System.out.println("Computer Brand"+brand);
+        System.out.println("Available Ports"+getPorts());
     }
 }
