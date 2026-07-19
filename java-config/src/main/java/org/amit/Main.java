bf8d6ae5f9ed597156d00b1e8a161b9a72fac407
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-19 19:45:11 +0530

    set scope for restaurant class as prototype

--- java-config/src/main/java/org/amit/Main.java
+++ java-config/src/main/java/org/amit/Main.java
@@ -7,8 +7,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       Restaurant res;
+       Restaurant res,res1;
        ApplicationContext con=new AnnotationConfigApplicationContext(Appconfig.class);
+        res1=(Restaurant) con.getBean("restaurant");
+        res1.nm="Non Amazing Restaurant";
+        res1.resturent();
        res=(Restaurant) con.getBean("restaurant");
        res.resturent();
     }
--- java-config/src/main/java/org/amit/Restaurant.java
+++ java-config/src/main/java/org/amit/Restaurant.java
@@ -6,14 +6,19 @@ import org.amit.kitchen.Kitchen;
 import org.amit.paymentgateway.PaymentGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Restaurant {
      Kitchen kit;
      DeliveryService del;
+     @Value("Amazing Restaurant")
+     String nm="";
      @Autowired
-             @Qualifier("cardPayment")
+     @Qualifier("cardPayment")
      PaymentGateway pay;
 
     @Autowired
@@ -32,7 +37,7 @@ public class Restaurant {
 
     public void resturent()
     {
-        System.out.println("This is An Amazing Restaurant");
+        System.out.println("This is An "+nm);
         kit.gettypeofkitchen();
         del.gettypeofdelivery();
         pay.getypeofpayment();
