82b9ae09a5b7c26eb784ba561a79cc533bae50ae
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-22 00:06:12 +0530

    implemnt pharmacy related classes

---
+++ annotation-config/src/main/java/pharmacy/InHousePharmacy.java
@@ -0,0 +1,8 @@
+package pharmacy;
+
+public class InHousePharmacy {
+    public void getpharmacytype()
+    {
+        System.out.println("This is an InHousePharmacy");
+    }
+}
---
+++ annotation-config/src/main/java/pharmacy/OutsourcedPharmacy.java
@@ -0,0 +1,8 @@
+package pharmacy;
+
+public class OutsourcedPharmacy implements Pharmacy{
+    public void getpharmacytype()
+    {
+        System.out.println("This is an OutsourcedPharmacy");
+    }
+}
