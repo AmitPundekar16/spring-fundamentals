26381684a9d23be25b4635124e6fbae95fa84ce5
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-24 17:00:07 +0530

    add project for practise of spring jdbc

---
+++ jdbc-basics/src/main/java/com/amit/jdbc_basics/JdbcBasicsApplication.java
@@ -0,0 +1,15 @@
+package com.amit.jdbc_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBasicsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JdbcBasicsApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ jdbc-basics/src/test/java/com/amit/jdbc_basics/JdbcBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.jdbc_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
