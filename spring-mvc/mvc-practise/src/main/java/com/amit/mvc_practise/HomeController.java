5e9a251b43122a7e637e4dc8c9c40f21cb4766a2
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-30 23:34:58 +0530

    send data between jsp pages using getparameter and HttpSession

--- spring-mvc/mvc-practise/src/main/java/com/amit/mvc_practise/HomeController.java
+++ spring-mvc/mvc-practise/src/main/java/com/amit/mvc_practise/HomeController.java
@@ -1,7 +1,10 @@
 package com.amit.mvc_practise;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +12,16 @@ public class HomeController {
     public String home()
     {
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession ses)
+    {
+        int n1 = Integer.parseInt(req.getParameter("num1"));
+        int n2 = Integer.parseInt(req.getParameter("num2"));
+        int res=n1+n2;
+
+        ses.setAttribute("res",res);
+       return "output.jsp";
     }
 }
--- spring-mvc/mvc-practise/src/main/webapp/index.jsp
+++ spring-mvc/mvc-practise/src/main/webapp/index.jsp
@@ -3,5 +3,17 @@
 <html>
   <body>
     <h1>Amit</h1>
+
+      <form action="add">
+            <label>First Number:</label>
+            <input type="number" name="num1" required>
+            <br><br>
+            <label>Second Number:</label>
+            <input type="number" name="num2" required>
+            <br><br>
+            <button type="submit">Add</button>
+
+        </form>
+
    </body>
  </html>
