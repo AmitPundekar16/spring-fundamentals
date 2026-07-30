bafe16cda0c636a0904e2c605ba5794dea65b9e7
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-30 16:12:53 +0530

    create repo to practise spring mvc

---
+++ spring-mvc/mvc-practise/src/main/java/com/amit/mvc_practise/MvcPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.amit.mvc_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcPractiseApplication.class, args);
+	}
+
+}
---
+++ spring-mvc/mvc-practise/src/test/java/com/amit/mvc_practise/MvcPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.mvc_practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
