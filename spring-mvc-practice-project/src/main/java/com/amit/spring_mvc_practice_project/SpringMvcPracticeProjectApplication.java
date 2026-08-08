2e96146ad73a75e6ebe9a3c8430cb9dfd83a3c88
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-08-08 13:11:15 +0530

    create directory to practise spring mvc

---
+++ spring-mvc-practice-project/src/main/java/com/amit/spring_mvc_practice_project/SpringMvcPracticeProjectApplication.java
@@ -0,0 +1,16 @@
+package com.amit.spring_mvc_practice_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPracticeProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringMvcPracticeProjectApplication.class, args);
+
+		System.out.println("hello");
+	}
+
+}
---
+++ spring-mvc-practice-project/src/test/java/com/amit/spring_mvc_practice_project/SpringMvcPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.spring_mvc_practice_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
